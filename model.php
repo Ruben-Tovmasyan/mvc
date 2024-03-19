8aa0144a4a153589eb56907fd3bca0c958d69869
Ruben__Tovmasyan <ruben.t@yoocollab.com>
2024-03-19 22:06:21 +0400

    Added model

---
+++ model.php
@@ -0,0 +1,12 @@
+<?php
+class UserModel {
+    private $username;
+
+    public function setUsername($username) {
+        $this->username = $username;
+    }
+
+    public function getUsername() {
+        return $this->username;
+    }
+}
